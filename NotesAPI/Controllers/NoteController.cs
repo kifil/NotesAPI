4f617a6223beb1ced144d4c02b30d435a7f7490a
DannyWilson <wilsondr16@gmail.com>
2017-05-17 23:21:38 -0500

    Inital project commit, near complete implementation

---
+++ NotesAPI/Controllers/NoteController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using NotesAPI.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace NotesAPI.Controllers
+{
+    //[Route("api/[controller]")]
+    [Route("api/notes")]
+    public class NoteController : Controller
+    {
+        private readonly INoteRepository _noteRepository;
+
+        public NoteController(INoteRepository noteRepository)
+        {
+            _noteRepository = noteRepository;
+        }
+
+        [HttpGet]
+        public IEnumerable<Note> GetAll(string query)
+        {
+            return _noteRepository.GetAllNotes(query);
+        }
+
+        [HttpGet("{id}", Name = "GetNoteById")]
+        public IActionResult GetNoteById(int id)
+        {
+            var item = _noteRepository.GetNoteById(id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+
+            //jsonresult?
+            return new ObjectResult(item);
+        }
+
+        [HttpPost]
+        public IActionResult Create([FromBody] Note note)
+        {
+            if (note == null)
+            {
+                return BadRequest();
+            }
+
+            _noteRepository.AddNote(note);
+
+            return CreatedAtRoute("GetNoteById", new { id = note.id }, note);
+        }
+
+    }
+}
---
+++ NotesAPI/Models/INoteRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NotesAPI.Models
+{
+    public interface INoteRepository
+    {
+        void AddNote(Note note);
+        IEnumerable<Note> GetAllNotes(string query);
+        Note GetNoteById(int id);
+    }
+}
