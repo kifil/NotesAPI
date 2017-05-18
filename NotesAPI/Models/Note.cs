4cc10536bd5a900acf4c3d0739719c2880266cf9
DannyWilson <wilsondr16@gmail.com>
2017-05-17 23:21:38 -0500

    Initial project commit, near complete implementation

---
+++ NotesAPI/Models/Note.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NotesAPI.Models
+{
+    public class Note
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public long id { get; set; }
+        public string body { get; set; }
+    }
+}
---
+++ NotesAPI/Models/NoteRepository.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NotesAPI.Models
+{
+    public class NoteRepository : INoteRepository
+    {
+        private readonly NoteContext _context;
+
+        public NoteRepository(NoteContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<Note> GetAllNotes(string query)
+        {
+            if (!String.IsNullOrEmpty(query))
+            {
+                return _context.Notes.Where(n => n.body.ToLower().Contains(query.ToLower())).ToList();
+            }
+
+            return _context.Notes.ToList();
+
+        }
+
+        public void AddNote(Note note)
+        {
+            _context.Notes.Add(note);
+            _context.SaveChanges();
+        }
+
+        public Note GetNoteById(int id)
+        {
+            return _context.Notes.FirstOrDefault(t => t.id == id);
+        }
+    }
+}
